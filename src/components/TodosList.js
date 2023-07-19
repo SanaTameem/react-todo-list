4a9fa5fa88d373c17663fb2bbfe0283f260bedda
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-07-19 10:19:41 +0430

    Add components folder

---
+++ src/components/TodosList.js
@@ -0,0 +1,71 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import TodoItem from './TodoItem';
+
+// function TodoList({
+//   todos, deleteItem, editItem, toggleCompleted,
+// }) {
+//   return (
+//     <>
+//       {todos.map((todo) => (
+//         <TodoItem
+//           key={todo.id}
+//           todo={todo}
+//           deleteItem={deleteItem}
+//           editItem={editItem}
+//           toggleCompleted={toggleCompleted}
+//         />
+//       ))}
+//     </>
+//   );
+// }
+// TodoList.propTypes = {
+//   todos: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.number.isRequired,
+//       title: PropTypes.string.isRequired,
+//       completed: PropTypes.bool.isRequired,
+//     }),
+//   ).isRequired,
+//   deleteItem: PropTypes.func.isRequired,
+//   editItem: PropTypes.func.isRequired,
+//   toggleCompleted: PropTypes.func.isRequired,
+// };
+// export default TodoList;
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import TodoItem from './TodoItem';
+
+function TodosList({
+  todos, deleteItem, editItem, toggleCompleted,
+}) {
+  return (
+    <>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          deleteItem={deleteItem}
+          editItem={editItem}
+          toggleCompleted={toggleCompleted}
+        />
+      ))}
+    </>
+  );
+}
+
+TodosList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      completed: PropTypes.bool.isRequired,
+    }),
+  ).isRequired,
+  deleteItem: PropTypes.func.isRequired,
+  editItem: PropTypes.func.isRequired,
+  toggleCompleted: PropTypes.func.isRequired,
+};
+
+export default TodosList;
